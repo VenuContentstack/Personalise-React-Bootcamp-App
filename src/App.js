aec50adbc8f9f9202883cc8802cdedf9efdb8cc7
Ashish Tiwari <ashish.tiwari@contentstack.com>
2024-08-29 14:40:46 +0530

    First Layout Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner/Banner';
+import Header from './components/Header/Header';
+
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Banner
+        title="Welcome to Our Website"
+        description="Discover amazing products and services."
+        buttonText="Learn More"
+        buttonLink="/about"
+        image="https://dev11-images.csnonprod.com/v3/assets/bltea7bdc3a9471a122/blt75fd0f20cb739021/66502ee071da6c0f78303a3f/Tokyo_Tower.webp?auto=webp&format=pjpg" // Replace with your image URL
+      />
     </div>
   );
 }
